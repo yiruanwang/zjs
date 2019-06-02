ac2b685b7a12bfbe421aed9db9565e6c543ba9ba
supbose <yshxw@qq.com>
2019-06-02 10:45:51 +0800

    1.1.109 moban

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-
+require("jquery");
 window.Zjs = window.$ = window.zjs = window.Yzjs = window.yzjs = window.Yz = window.yz = window.$$app = require("./js/zjs.js");
+console.log(`${'\n'} %c Qsjs ${VERSION}  %c http://qsjs.yiruan.wang ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const gitRevisionPlugin = new GitRevisionPlugin();
 
 module.exports = {
     entry: './src/index.js',
@@ -9,16 +12,49 @@ module.exports = {
         filename: 'js/zjs.min.js',
         path: path.resolve(__dirname, 'dist')
     }, plugins: [
-        // new UglifyJsPlugin(),
+        new UglifyJsPlugin(),
         new CopyWebpackPlugin(
             [
                 {from: 'favicon.ico', to: 'favicon.ico'},
-                {from: 'example/index.html', to: 'index.html'},
+                // {from: 'example/index.html', to: 'index.html'},
                 {from: 'static', to: 'static'},
             ],
             {ignore: [], copyUnmodified: true}
         ),
-    ], devServer: {
+        new HtmlWebpackPlugin({
+            minify:{
+                collapseWhitespace:false //折叠空白区域 也就是压缩代码
+            },
+            hash:true,
+            inject:'head',
+            title:'播放器',
+            template: './example/index.html', //模板地址
+            filename: 'index.html',
+            //template: 'm_vol.html'
+        }),
+        gitRevisionPlugin,
+        new webpack.DefinePlugin({
+            'VERSION': JSON.stringify(gitRevisionPlugin.version()),
+            'QSJS-VERSION': JSON.stringify(gitRevisionPlugin.version()),
+            'COMMITHASH': JSON.stringify(gitRevisionPlugin.commithash()),
+            'BRANCH': JSON.stringify(gitRevisionPlugin.branch()),
+        })
+    ], module: {
+        rules: [
+            //暴露$和jQuery到全局
+            {
+                test: require.resolve('jquery'), //require.resolve 用来获取模块的绝对路径
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                }, {
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            }
+        ]
+    },
+ devServer: {
         port: 7000
     },
     performance: {
